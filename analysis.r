1a7694729f0e4b638f7f3f2ba82b344b24a187a4
hongyusu <hongyu.su.uh@gmail.com>
2012-10-29 14:19:53 +0200

    2910

--- analysis.r
+++ analysis.r
@@ -1,32 +1,32 @@
 
 
 
-samples=c('emotions','yeast','scene','enron','medical','toy10','toy50')
+samples=c('emotions','yeast','scene','enron','cal500','fp','cancer','medical','toy10','toy50')
 #samples=c('toy10')
 
 pdf(sprintf('../plots/plot.pdf'),height=4*length(samples),width=20)
 par(mfrow=c(length(samples),5))
 
-for(sample in samples)
+for(sam in samples)
 {
     d=c()
-    tmp=as.matrix(read.table(sprintf('../results/%s_perfSVM',sample),sep=','))   
+    tmp=as.matrix(read.table(sprintf('../results/%s_perfSVM',sam),sep=','))   
     d=rbind(d,tmp[2,])
-    tmp=as.matrix(read.table(sprintf('../results/%s_perfBagSVM',sample),sep=','))   
+    tmp=as.matrix(read.table(sprintf('../results/%s_perfBagSVM',sam),sep=','))   
     d=rbind(d,tmp[2,])
-    tmp=as.matrix(read.table(sprintf('../results/%s_perfAda',sample),sep=','))   
+    tmp=as.matrix(read.table(sprintf('../results/%s_perfAda',sam),sep=','))   
     d=rbind(d,tmp[2,])
-    tmp=matrix(rep(NA,14),2,7);try(tmp=as.matrix(read.table(sprintf('../results/%s_perfMTL',sample),sep=','))) 
+    tryCatch({tmp=as.matrix(read.table(sprintf('../results/%s_perfMTL',sam),sep=','))},error=function(e) tmp=matrix(rep(NA,14),2,7)) 
     d=rbind(d,tmp[2,])
-    tmp=matrix(rep(NA,35),5,7);try(tmp=as.matrix(read.table(sprintf('../results/%s_perfPairEns',sample),sep=','))) 
+    tryCatch({tmp=as.matrix(read.table(sprintf('../results/%s_perfPairEns',sam),sep=','))},error=function(e) tmp=matrix(rep(NA,35),5,7)) 
     d=rbind(d,tmp[-1,])
-    tmp=matrix(rep(NA,35),5,7);try(tmp=as.matrix(read.table(sprintf('../results/%s_perfTreeEns',sample),sep=',')))   
+    tryCatch({tmp=as.matrix(read.table(sprintf('../results/%s_perfTreeEns',sam),sep=','))},error=function(e) tmp=matrix(rep(NA,35),5,7)) 
     d=rbind(d,tmp[-1,])
     rownames(d)=c('SVM','Bag','Ada','MTL','PairRand','PairBin','PairVal','PairMad','TreeRand','TreeBin','TreeVal','TreeMad') 
     colnames(d)=c('Acc','Vec','Pre','Rec','F1','AUC1','AUC2')
 
     for(i in c(1,2,5,6,7))
-    {plot(d[,i],main=sprintf("%s",sample),ylab=sprintf('%s',colnames(d)[i]))}
+    {plot(d[,i],main=sprintf("%s",sam),ylab=sprintf('%s',colnames(d)[i]))}
 }
 
 dev.off()
